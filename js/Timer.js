1c29d553170408eb84f1e191b6cc4ced39b3bbda
Nma Mbeledogu <ncmbeledogu@gmail.com>
2024-03-27 09:52:56 -0400

    Refactor Timer and Interval classes

--- js/Timer.js
+++ js/Timer.js
@@ -1,42 +1,42 @@
-import Interval from "./Interval.js";
-
 export default class Timer {
   constructor() {
     this.intervals = new Array();
-    this.remainingSecondsTotal = 0;
+    this.remainingSeconds = 0;
     this.interval = null;
   }
 
   addInterval(hours, minutes, seconds) {
-    const interval = new Interval(hours, minutes, seconds);
+    const interval = new Timer.Interval(hours, minutes, seconds);
 
     const MAXSECONDS = 359999;
-    if (this.remainingSecondsTotal + interval.remainingSeconds > MAXSECONDS) {
-      const truncatedIntervalSeconds = MAXSECONDS - this.remainingSecondsTotal;
+    if (this.remainingSeconds + interval.remainingSeconds > MAXSECONDS) {
+      const truncatedIntervalSeconds = MAXSECONDS - this.remainingSeconds;
       if (truncatedIntervalSeconds > 0) {
         interval.remainingSeconds = truncatedIntervalSeconds;
         this.intervals.push(interval);
-      }
+    }
 
-      this.remainingSecondsTotal = MAXSECONDS;
-    } else {
-      this.remainingSecondsTotal += interval.remainingSeconds;
+      this.remainingSeconds = MAXSECONDS;
+    }
+    else {
+      this.remainingSeconds += interval.remainingSeconds;
       this.intervals.push(interval);
     }
   }
 
-  syncInterface(hoursElement, minutesElement, secondsElement) {
-  const hours = Math.floor(this.remainingSecondsTotal / 3600);
-  const minutes = Math.floor(this.remainingSecondsTotal / 60) - 60 * hours;
-  const seconds = this.remainingSecondsTotal % 60;
-
-  hoursElement.textContent = hours.toString().padStart(2, "0");
-  minutesElement.textContent = minutes.toString().padStart(2, "0");
-  secondsElement.textContent = seconds.toString().padStart(2, "0");
-}
-
   pop() {
     this.intervals.shift();
     return this.intervals[0];
+  }
+}
+
+Timer.Interval = class {
+  constructor(hours, minutes, seconds) {
+    this.remainingSeconds = 3600 * hours + 60 * minutes + seconds;
+
+    const MAXSECONDS = 359999;
+    if (this.remainingSeconds > MAXSECONDS) {
+      this.remainingSeconds = MAXSECONDS;
+    }
   }
 }
